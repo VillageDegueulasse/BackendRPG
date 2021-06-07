b1a315470689acf980f70a6cfa01fb3de64c1ae7
PsychiatrieTv <psychiatrietv02@gmail.com>
2021-06-07 13:48:12 +0200

    connection db ok

--- BackendRPG/Backend/TPK-TS-NODE-BOILERPLATE/src/app.bootstrap.ts
+++ BackendRPG/Backend/TPK-TS-NODE-BOILERPLATE/src/app.bootstrap.ts
@@ -1,5 +1,33 @@
-function initApp() {
-    console.log('Init application');
-}
+/* eslint-disable no-console */
+import express, { Request, Response } from 'express';
+import { createConnection } from 'typeorm';
 
-initApp();
+
+
+
+
+createConnection().then(async connection => {
+   
+    
+    const app = express();
+    const port = 8000;
+    app.get('/rpg', (req : Request, res : Response) => {
+        res.send('isoké');
+    });
+    
+    
+
+    if(connection){
+        // const result = await connection.query('SHOW DATABASES');
+        console.log();
+        
+        app.listen(port, () => {
+            console.log(`localhost a démarré sur : http://localhost:${port}`);
+            
+        });
+    } else {
+        console.log('il y a problème');        
+    }
+    
+}).catch(error => console.log(error));
+
