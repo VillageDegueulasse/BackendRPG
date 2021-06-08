87d04a76d3ee7379bdefe112b305f12902682b8f
marinebel <marinebeltus@gmail.com>
2021-06-08 16:05:08 +0200

    validators

--- BackendRPG/Backend/template/src/routes/stuff.route.ts
+++ BackendRPG/Backend/template/src/routes/stuff.route.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { StuffController } from '../controllers/stuff.controller';
+import { StuffValidator } from '../validators/stuff.validator';
 
 const stuffRouter = Router();
 
 stuffRouter.get('/api/v1/stuff', StuffController.getAll);
 stuffRouter.get('/api/v1/stuff/:id', StuffController.getById);
-stuffRouter.post('/api/v1/stuff', StuffController.create);
-stuffRouter.put('/api/v1/stuff/:id', StuffController.update);
+stuffRouter.post('/api/v1/stuff', StuffValidator.getRules(), StuffValidator.validate, StuffController.create);
+stuffRouter.put('/api/v1/stuff/:id', StuffValidator.getRules(), StuffValidator.validate, StuffController.update);
 stuffRouter.delete('/api/v1/stuff/:id', StuffController.delete);
 
 export { stuffRouter };
