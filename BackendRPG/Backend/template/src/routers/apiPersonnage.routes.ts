f0c0c20e18470cbf6df779aa7f025ce7df64133d
marinebel <marinebeltus@gmail.com>
2021-06-10 15:37:41 +0200

    add validators

--- BackendRPG/Backend/template/src/routers/apiPersonnage.routes.ts
+++ BackendRPG/Backend/template/src/routers/apiPersonnage.routes.ts
@@ -6,19 +6,11 @@ import { PersonnageValidator } from '../validators/personnage.validator';
 
 const apiPersonnageRouter = Router();
 
-<<<<<<< HEAD
 apiPersonnageRouter.post('/api/v1/create', PersonnageValidator.getRules(), PersonnageValidator.validate, catchErrors(PersonnageController.create));
 apiPersonnageRouter.get('/api/v1/personnage', catchErrors(PersonnageController.findAll));
 apiPersonnageRouter.get('/api/v1/personnage/:id', catchErrors(PersonnageController.findById));
 apiPersonnageRouter.delete('/api/v1/deletePersonnage/:id', PersonnageValidator.getRules(), PersonnageValidator.validate, catchErrors(PersonnageController.delete));
 apiPersonnageRouter.put('/api/v1/updatePersonnage/:id', PersonnageValidator.getRules(), PersonnageValidator.validate, catchErrors(PersonnageController.update));
-=======
-apiPersonnageRouter.post('/api/v1/createPersonnage', catchErrors(PersonnageController.create));
-apiPersonnageRouter.get('/api/v1/personnage', catchErrors(PersonnageController.findAll));
-apiPersonnageRouter.get('/api/v1/personnageByid/:id', catchErrors(PersonnageController.findById));
-apiPersonnageRouter.delete('/api/v1/deletePersonnage/:id', catchErrors(PersonnageController.delete));
-apiPersonnageRouter.put('/api/v1/updatePersonnage/:id', catchErrors(PersonnageController.update));
->>>>>>> d4caeaa8dc7c402fc97c7841d0157762b92f8b2f
 
 
 export {apiPersonnageRouter};
