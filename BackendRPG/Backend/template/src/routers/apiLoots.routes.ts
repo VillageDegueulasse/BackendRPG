f789678aa814789c4956ddd0b27da72fe4323792
PsychiatrieTv <psychiatrietv02@gmail.com>
2021-06-10 15:38:34 +0200

    ok

--- BackendRPG/Backend/template/src/routers/apiLoots.routes.ts
+++ BackendRPG/Backend/template/src/routers/apiLoots.routes.ts
@@ -10,7 +10,7 @@ const apiLootRouter = Router();
 apiLootRouter.post('/api/v1/createLoot', catchErrors(LootController.create));
 apiLootRouter.get('/api/v1/Loots', catchErrors(LootController.findAll));
 apiLootRouter.put('/api/v1/updateLoots/:id', catchErrors(LootController.update));
-apiLootRouter.get('/api/v1/deleteLoots/:id', catchErrors(LootController.delete));
+apiLootRouter.delete('/api/v1/deleteLoots/:id', catchErrors(LootController.delete));
 apiLootRouter.get('/api/v1/lootById/:id', catchErrors(LootController.findById));
 //TEST
 // apiLootRouter.get('/api/v1/test/:id', catchErrors(LootController.findLootByMonsterId));
